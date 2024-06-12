a8d8e0a812af3befc08376ad85040672f7bc988e
it <kyle9742@naver.com>
2024-06-12 10:05:11 +0900

    first commit 카드 섞기

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import './App.css'
+
+const cardImages = [
+  { "src": "/img/helmet-1.png"}, 
+  { "src": "/img/potion-1.png"}, 
+  { "src": "/img/ring-1.png"}, 
+  { "src": "/img/scroll-1.png"}, 
+  { "src": "/img/shield-1.png"}, 
+  { "src": "/img/sword-1.png"}, 
+]
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0); 
+  const shuffledCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random()-0.5) // .sort 는 정렬
+    .map((card) => ({...card, id: Math.random()}));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
+  return (
+    <>
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffledCards}>New Game</button>
+    </div>
+    <div className='card-grid'>
+      {cards.map(card => (
+        <div className='card' key={card.id}>
+          <div>
+            <img className='front' src={card.src} alt='card front'></img>
+            <img className='back' src='/img/cover.png' alt='card back'></img>
+          </div>
+        </div>
+      ))}
+    </div>
+    </>
+  );
+}
+
+export default App
