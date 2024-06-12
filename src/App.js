4c0c7d62e5bf7683ecf2f5fd1046e4ea5e15a6a0
it <kyle9742@naver.com>
2024-06-12 12:02:45 +0900

    css 효과까지 완성

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 import SingleCard from "./componets/SingleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png", matched : false },
+  { src: "/img/potion-1.png", matched : false },
+  { src: "/img/ring-1.png", matched : false },
+  { src: "/img/scroll-1.png", matched : false },
+  { src: "/img/shield-1.png", matched : false },
+  { src: "/img/sword-1.png", matched : false },
 ];
+
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
+  
   const shuffledCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5) // .sort 는 정렬
@@ -29,11 +31,18 @@ function App() {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   }
 
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prev) => prev + 1);
+  };
+
   useEffect(() => {
     if (choiceOne && choiceTwo) {
+      setDisabled(true);
       if (choiceOne.src === choiceTwo.src) {
         setCards((prevCards) => {
-          return prevCards.map((card) => {
+          return prevCards.map(card => {
             if (card.src === choiceOne.src) {
               return { ...card, matched: true };
             } else {
@@ -43,20 +52,12 @@ function App() {
         });
         resetTurn();
       } else {
-        console.log("틀렸네요.");
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000);
       }
     }
   }, [choiceOne, choiceTwo]);
 
-  const resetTurn = () => {
-    setChoiceOne(null);
-    setChoiceTwo(null);
-    setTurns((prev) => prev + 1);
-  };
-
   
-
   return (
     <>
       <div className="App">
@@ -65,7 +66,10 @@ function App() {
 
         <div className="card-grid">
           {cards.map((card) => (
-            <SingleCard handleChoice={handleChoice} card={card} key={card.id} />
+            <SingleCard 
+            handleChoice={handleChoice} 
+            card={card} key={card.id} 
+            flipped={card === choiceOne || card === choiceTwo || card.matched}/>
           ))}
         </div>
       </div>
